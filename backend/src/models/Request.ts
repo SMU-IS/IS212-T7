9b2d5bef16b934d7b55563df144238a9b5a8331b
bryantheball <bryanelijah.18@gmail.com>
2024-09-19 15:19:13 +0800

    feat: added base code and dummy data for request

--- backend/src/models/Request.ts
+++ backend/src/models/Request.ts
@@ -1,34 +1,68 @@
-import { RequestDay, RequestType, Status } from "@/helpers";
-import mongoose, { Date } from "mongoose";
+import { RequestType, Status } from "@/helpers";
+import mongoose from "mongoose";
 
 interface IRequest {
   requestId: number;
   requestType: RequestType;
   requestedDate: Date;
-  requestedDay: RequestDay;
   reason: string;
   assignedTo: number;
-  approvedOn: Date;
+  requestedBy: number;
   status: Status;
 }
 
 const Schema = mongoose.Schema;
+
+// Counter Schema to allow auto increment of RequestId
+const CounterSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  seq: { type: Number, default: 0 },
+});
+
+const Counter = mongoose.model("Counter", CounterSchema);
+
+async function initializeCounter() {
+  const counter = await Counter.findById("requestId");
+  if (!counter) {
+    await new Counter({ _id: "requestId", seq: 0 }).save();
+  }
+}
+
+// Initialize counter schema if it doesnt already exist
+initializeCounter().catch(console.error);
+
 const RequestSchema = new Schema<IRequest>(
   {
-    requestId: { type: Number, unique: true, required: true },
+    requestId: { type: Number, unique: true },
     requestType: { type: String, required: true },
     requestedDate: { type: Date, required: true },
-    requestedDay: { type: String, required: true },
     reason: { type: String, required: true },
     assignedTo: { type: Number, required: true },
-    approvedOn: { type: Date, required: true },
-    status: { type: String, required: true },
+    requestedBy: { type: Number, required: true },
+    status: {
+      type: String,
+      required: true,
+      enum: ["PENDING", "APPROVED", "REJECTED", "CANCELLED", "WITHDRAWN"],
+      default: Status.PENDING,
+    },
   },
   {
     timestamps: true,
   }
 );
 
-RequestSchema.index({ requestId: 1 }, { unique: true });
+// set requestId to current counter + 1
+RequestSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    const counter = await Counter.findByIdAndUpdate(
+      "requestId",
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+    this.requestId = counter.seq;
+  }
+  next();
+});
 
+RequestSchema.index({ requestId: 1 }, { unique: true });
 export default mongoose.model<IRequest>("Request", RequestSchema);
