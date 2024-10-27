58d128cc3952345ad8e831dda4975719540c7bdb
lurence <46553691+anonymouspanda-23@users.noreply.github.com>
2024-10-28 03:24:47 +0800

    chore: added mock server to enable api calls for integration test.

--- backend/src/controllers/EmployeeController.integration.test.ts
+++ backend/src/controllers/EmployeeController.integration.test.ts
@@ -1,14 +1,13 @@
-import { Context } from 'koa';
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import EmployeeController from '@/controllers/EmployeeController';
-import EmployeeService from '@/services/EmployeeService';
-import EmployeeDb from '@/database/EmployeeDb';
 import Employee from '@/models/Employee';
-import { errMsg } from "@/helpers";
 import path from "path";
 import { readFileSync } from "fs";
 import { hashPassword } from "@/tests/utils";
+import { app } from "@/index";
+import request from "supertest";
+import { Server } from "http";
+import { errMsg } from "@/helpers";
 
 // Unmock mongoose and Employee model specifically for this test file
 jest.unmock('mongoose');
@@ -16,8 +15,7 @@ jest.unmock('@/models/Employee');
 
 describe('Employee Integration Tests', () => {
   let mongoServer: MongoMemoryServer;
-  let employeeController: EmployeeController;
-  let ctx: Context;
+  let mockServer: Server;
 
   const filePath = path.resolve("@/../script/employee.json");
   const fileContent = readFileSync(filePath, "utf-8");
@@ -31,10 +29,7 @@ describe('Employee Integration Tests', () => {
     // Connect to the in-memory database
     await mongoose.connect(mongoUri);
 
-    // Initialize the controller with its dependencies
-    const employeeDb = new EmployeeDb();
-    const employeeService = new EmployeeService(employeeDb);
-    employeeController = new EmployeeController(employeeService);
+    mockServer = app.listen();
   });
 
   afterAll(async () => {
@@ -46,7 +41,7 @@ describe('Employee Integration Tests', () => {
     // Clear all collections
     await Employee.deleteMany();
 
-    const EMPLOYEE_LIMIT = 50; // Adjust this value as needed
+    const EMPLOYEE_LIMIT = 10; // Adjust this value as needed
 
     // Populate table
     for (let i = 0; i < Math.min(EMPLOYEE_LIMIT, employees.length); i++) {
@@ -54,27 +49,16 @@ describe('Employee Integration Tests', () => {
       employeeData.hashedPassword = await hashPassword("password123");
       await Employee.create(employeeData);
     }
-
-    // Reset mock context
-    ctx = {
-      method: 'POST',
-      query: {},
-      body: {},
-      request: { body: {} },
-      response: {},
-    } as Context;
-  }, 60000); // Run for 1 min.
+  }, 60000); // Set limit to 1 min. Default is 5 sec.
 
   describe('getEmployeeByEmail', () => {
     it('should return employee data when credentials are valid', async () => {
-      ctx.request.body = {
+      const requestBody = {
         staffEmail: 'jack.sim@allinone.com.sg',
         staffPassword: 'password123'
       };
 
-      await employeeController.getEmployeeByEmail(ctx);
-
-      expect(ctx.body).toMatchObject({
+      const expectedResponse = {
         staffId: 130002,
         name: 'Jack Sim',
         dept: 'CEO',
@@ -82,20 +66,32 @@ describe('Employee Integration Tests', () => {
         email: 'jack.sim@allinone.com.sg',
         reportingManager: 130002,
         role: 1,
-      });
+      };
+
+      const response = await request(mockServer)
+        .post("/api/v1/login")
+        .send(requestBody)
+
+      expect(response.status).toBe(200);
+      expect(response.body).toStrictEqual(expectedResponse);
     });
 
     it('should return error for non-existent user', async () => {
-      ctx.request.body = {
+      const requestBody = {
         staffEmail: 'nonexistent@lurence.org',
         staffPassword: 'test-password'
       };
 
-      await employeeController.getEmployeeByEmail(ctx);
+      const expectedResponse = {
+        "error": errMsg.USER_DOES_NOT_EXIST
+      };
 
-      expect(ctx.body).toEqual({
-        error: errMsg.USER_DOES_NOT_EXIST
-      });
+      const response = await request(mockServer)
+        .post("/api/v1/login")
+        .send(requestBody)
+
+      expect(response.status).toBe(200);
+      expect(response.body).toStrictEqual(expectedResponse);
     });
   });
 });
